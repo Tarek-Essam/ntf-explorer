ec1fff90cf00ea5257b8e614bbf41fe9cfe867cf
Tarek-Essam <tarekessam1311@gmail.com>
2023-01-03 14:05:35 +0400

    feat: add swagger docs only in dev environment

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,12 +1,12 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { appConfig } from '@src/env';
 
 export function configureSwagger(app: INestApplication) {
+  if (!appConfig.isDev) return;
   const config = new DocumentBuilder()
     .setTitle('NFT Explorer')
-    .setDescription(
-      'APIs to help explore the nft tokens owners & account balances',
-    )
+    .setDescription('APIs to help explore the nft tokens owners & account balances')
     .setVersion('1.0')
     .build();
 
